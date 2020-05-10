f4b25f3b2d7c454692739d1e2c487fd8d0509dd1
ynfle <23086821+ynfle@users.noreply.github.com>
2020-05-10 19:00:00 -0400

    Feature: Add JSON formatter for `unittest` (#2)
    This commit adds `src/unittest_json.nim` - a module that extends
    `unittest` to output a test report in the JSON format specified by:
    https://github.com/exercism/automated-tests/blob/master/docs/interface.md
    
    The current implementation produces correct output for at least simple
    `pass` and `fail` cases, and allows writing tests that help improve the
    behavior in more complex cases.
    
    Co-authored-by: ee7 <45465154+ee7@users.noreply.github.com>

---
+++ src/unittest_json.nim
@@ -0,0 +1,116 @@
+## An extension of the ``unittest.nim`` library to output json for Exercism v3
+
+import json, streams, strformat, unittest
+
+type
+  JsonTestStatus = enum
+    PASS = "pass",
+    FAIL = "fail",
+    ERROR = "error"
+
+  JsonTestResult = object
+    name: string
+    output: string
+    case status: JsonTestStatus
+    of FAIL, ERROR:
+      message: string
+    of PASS:
+      discard
+
+  ResultJson = ref object
+    tests: seq[JsonTestResult]
+    case status: JsonTestStatus
+    of ERROR:
+      message: string
+    of PASS, FAIL:
+      discard
+
+  JsonOutputFormatter = ref object of OutputFormatter
+    stream: Stream
+    testErrors: seq[string]
+    testStackTrace: string
+    result: ResultJson
+
+proc newJsonOutputFormatter*(stream: Stream): <//>JsonOutputFormatter =
+  ## Creates a formatter that writes report to the specified stream in
+  ## JSON format.
+  ## The ``stream`` is NOT closed automatically when the test are finished,
+  ## because the formatter has no way to know when all tests are finished.
+  ## You should invoke formatter.close() to finalize the report.
+  result = JsonOutputFormatter(
+    stream: stream,
+    testErrors: @[],
+    testStackTrace: "",
+    result: ResultJson(tests: @[], status: PASS)
+  )
+
+proc close*(formatter: JsonOutputFormatter) =
+  ## Completes the report and closes the underlying stream.
+  formatter.stream.write($(%formatter.result))
+  formatter.stream.close()
+
+method suiteStarted(formatter: JsonOutputFormatter, suiteName: string) =
+  discard
+
+method testStarted(formatter: JsonOutputFormatter, testName: string) =
+  formatter.testErrors.setLen(0)
+  formatter.testStackTrace.setLen(0)
+
+method failureOccurred(formatter: JsonOutputFormatter,
+                       checkpoints: seq[string], stackTrace: string) =
+  ## ``stackTrace`` is provided only if the failure occurred due to an
+  ## exception. ``checkpoints`` is never ``nil``.
+  formatter.testErrors.add(checkpoints)
+  if stackTrace.len > 0:
+    formatter.testStackTrace = stackTrace
+
+method testEnded(formatter: JsonOutputFormatter, testResult: TestResult) =
+  var jsonTestResult: JsonTestResult
+  case testResult.status
+  of TestStatus.OK:
+    jsonTestResult = JsonTestResult(name: testResult.testName, status: PASS)
+  of TestStatus.SKIPPED:
+    discard
+  of TestStatus.FAILED:
+    let failureMsg =
+      if formatter.testStackTrace.len > 0 and formatter.testErrors.len > 0:
+        formatter.testErrors[^1]
+      elif formatter.testErrors.len > 0:
+        formatter.testErrors[0]
+      else:
+        "The test failed without outputting an error"
+
+    var errs = ""
+    if formatter.testErrors.len > 1:
+      let startIdx = if formatter.testStackTrace.len > 0: 0 else: 1
+      let endIdx =
+        if formatter.testStackTrace.len > 0:
+          formatter.testErrors.len - 2
+        else:
+          formatter.testErrors.len - 1
+
+      for errIdx in startIdx..endIdx:
+        if errs.len > 0:
+          errs.add("\n")
+        errs.add(formatter.testErrors[errIdx])
+
+    if formatter.testStackTrace.len > 0:
+      jsonTestResult = JsonTestResult(
+        name: testResult.testName,
+        status: FAIL,
+        message: fmt"{failureMsg}\n{formatter.testStackTrace}"
+      )
+      if errs.len > 0:
+        jsonTestResult.message.insert(errs & "\n")
+    else:
+      jsonTestResult = JsonTestResult(
+        name: testResult.testName,
+        status: FAIL,
+        message: fmt"{failureMsg}\n{errs}"
+      )
+    formatter.result = ResultJson(status: FAIL, tests: formatter.result.tests)
+
+  formatter.result.tests.add(jsonTestResult)
+
+method suiteEnded(formatter: JsonOutputFormatter) =
+  discard
