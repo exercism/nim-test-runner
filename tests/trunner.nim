e102430db7f0620ed409dccc1f2b8b24a28eab72
ee7 <45465154+ee7@users.noreply.github.com>
2020-05-16 20:00:03 +0200

    Refactor(trunner): Move `run all` test to its own file
    This disables the test for CI.
    
    One useful function of this test is to check that `pcre` is available,
    since some of the `exercism/nim` solutions use the `re` module. But we
    should test that in a more direct and time-efficient way.

--- tests/trunner.nim
+++ tests/trunner.nim
@@ -1,4 +1,4 @@
-import critbits, json, os, osproc, strutils, unittest
+import json, os, strutils, unittest
 import runner
 
 let tmpBase = getTempDir()
@@ -61,49 +61,3 @@ for status in ["pass", "fail", "error"]:
               else:
                 echo "Missing: " & pathExpectedResultsJson
               fail()
-
-suite "run all":
-  let baseDir = getTempDir() / "exercism" / "nim"
-  if not existsDir(baseDir):
-    let cmd = "git clone --depth 1 https://github.com/exercism/nim.git " &
-              baseDir
-    let errC = execCmd(cmd)
-    if errC != 0:
-      echo "Error: failed when running `git clone`"
-      removeDir(baseDir)
-      quit(1)
-
-  var slugs: CritBitTree[void]
-
-  let exercisesDir = baseDir / "exercises"
-  for (_, dir) in walkDir(exercisesDir):
-    slugs.incl(dir.splitPath().tail)
-
-  # Run the tests in alphabetical order
-  for slug in slugs:
-    test slug:
-      let slugDir = exercisesDir / slug
-      let slugUnder = slug.replace('-', '_')
-      let exampleContents = readFile(slugDir / "example.nim")
-      let solutionPath = slugDir / slugUnder & ".nim"
-      writeFile(solutionPath, exampleContents)
-
-      let conf = Conf(slug: slug,
-                      inputDir: slugDir,
-                      outputDir: outputDir)
-
-      let tmpDir = createTmpDir()
-      let testPath = prepareFiles(conf, tmpDir)
-      copyFile(getAppDir().parentDir / "src" / "unittest_json.nim", tmpDir / "unittest_json.nim")
-      discard run(testPath)
-      let resultsPath = conf.outputDir / "results.json"
-      let j = parseFile(resultsPath)
-      for test in j["tests"]:
-        check:
-          test["name"].getStr().len > 0
-          test["output"].getStr().len == 0
-          test["status"].getStr() == "pass"
-          test.len == 3
-      check:
-        j["status"].getStr() == "pass"
-      moveFile(resultsPath, conf.outputDir / slugUnder & ".json")
