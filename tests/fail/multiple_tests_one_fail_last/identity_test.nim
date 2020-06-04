9b2ef4f81168528c48894c441f507cf6409bfa26
ee7 <45465154+ee7@users.noreply.github.com>
2020-06-04 20:00:03 +0200

    Test: Add `fail/multiple_tests` cases
    This commit adds the first test cases for "multiple tests, with at least
    one failure". We add 5 test files, each with 3 tests.
    
    The 5 cases are:
    - 3 tests fail
    - 2 tests fail
    - 1 test fails (the first one)
    - 1 test fails (the middle one)
    - 1 test fails (the last one)
    
    As mentioned in the previous commit, we should later improve the
    `message` values by stripping paths and correcting line numbers.

---
+++ tests/fail/multiple_tests_one_fail_last/identity_test.nim
@@ -0,0 +1,12 @@
+import unittest
+import identity
+
+suite "Identity Function":
+  test "identity function of 1":
+    check identity(1) == 1
+
+  test "identity function of 2":
+    check identity(2) == 2
+
+  test "identity function of 3":
+    check identity(3) == 3000
