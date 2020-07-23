bb103bf624781cc646bea9b74f3a48c4a5efd1c9
ee7 <45465154+ee7@users.noreply.github.com>
2020-07-23 20:00:07 +0200

    Test: Improve `pass/multiple_tests_output_truncated`
    Changes:
    - Check the truncation behaviour for 499, 500 and 501 characters,
      rather than just 500, making use of the multiple tests. We can see
      this is useful because the initial implementation had an off-by-one
      error: output of exactly rune-length 500 did output all 500 runes,
      but then said incorrectly that the output was truncated. This is
      fixed in a later commit.
    - Use digits rather than letters, which makes it easier to recognise the
      length of the strings in the test file. In particular, it was
      previously unexpected that an apparent `a..z` omits the letter `f` -
      it was confusing that the string ended in "z... output is truncated",
      because the alphabet is 26 letters long and 500 isn't divisible by 26.
    
    I think `_123456789` is clearer than the full `a..z`: because with
    "xyzabcdef... output is truncated" it's less obvious that 'f' is the
    500th character. The reader shouldn't need to divide 500 by 26 and work
    out the remainder is 6 to see that 'f', as the 6th letter of the
    alphabet, is correct.
    
    Using `_123456789` also improves consistency if we use "unicode numbers"
    in the unicode truncation test.

--- tests/pass/multiple_tests_output_truncated/identity.nim
+++ tests/pass/multiple_tests_output_truncated/identity.nim
@@ -1,5 +1,14 @@
 import strutils
 
-func identity*(n: int): int =
-  debugEcho "abcdeghijklmnopqrstuvwxyz".repeat(20)
-  n
+const longString = "_123456789".repeat(50)
+doAssert longString.len == 500
+
+proc identity*(n: int): int =
+  if n == 1:
+    echo longString[0 .. ^3] # len of 499, including '\n' from `echo`
+  elif n == 2:
+    echo longString[0 .. ^2] # len of 500, including '\n' from `echo`
+  elif n == 3:
+    echo longString          # len of 501, including '\n' from `echo`
+
+  result = n
