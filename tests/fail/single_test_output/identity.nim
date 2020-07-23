16bdf5fb2fd8cfdb84d9c6e3d9bcf45a7da798e7
ee7 <45465154+ee7@users.noreply.github.com>
2020-07-23 20:00:04 +0200

    Test: Use `proc` instead of `func` and `debugEcho`
    Nim considers writing to stdout/stderr as a side effect - I think it's
    better to just use `proc` and more quickly signal that to the reader;
    it's probably better to save a `{.noSideEffect.}` block for more special
    occasions.

--- tests/fail/single_test_output/identity.nim
+++ tests/fail/single_test_output/identity.nim
@@ -1,6 +1,5 @@
-func identity*(n: int): int =
-  debugEcho "stdout here: my input is: " & $n
-  {.noSideEffect.}:
-    stderr.writeLine "stderr here: my input is: " & $n
-    stderr.flushFile
-  n
+proc identity*(n: int): int =
+  echo "stdout here: my input is: " & $n
+  stderr.writeLine "stderr here: my input is: " & $n
+  stderr.flushFile
+  result = n
